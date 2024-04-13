19524215d9818bc0b58a8034be1c8e10c4be134a
aliifam <aliif.arief.m@gmail.com>
2024-04-13 21:09:02 +0700

    feat: add openapi automatic register

--- src/docs/openapi.js
+++ src/docs/openapi.js
@@ -6,8 +6,19 @@ const options = {
     openapi: "3.0.0",
     info: {
       title: "PDU API Documentation",
+      description: "PDU API Documentation",
       version: "1.0.0",
     },
+    servers: [
+      {
+        url: "http://localhost:3000",
+        description: "Development server",
+      },
+      {
+        url: "https://pdu-api.web.app",
+        description: "Production server",
+      },
+    ],
   },
   apis: ["./src/router/*.routes.js"],
 };
--- src/router/public.routes.js
+++ src/router/public.routes.js
@@ -10,17 +10,50 @@ const publicRouter = new express.Router();
 
 //employee public router
 /**
+ * @openapi
+ * /api/employee/register:
+ *   post:
+ *     tags:
+ *       - Employee
+ *     summary: Register new employee auto assign role as admin
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *               - password
+ *               - confirmPassword
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 maxLength: 100
+ *                 default: john doe
+ *               email:
+ *                 type: string
+ *                 maxLength: 100
+ *                 default: jd@pdu.com
+ *               password:
+ *                 type: string
+ *                 pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{6,})"
+ *               confirmPassword:
+ *                 type: string
+ *                 ref: password
+ *     example:
+ *       name: John Doe
+ *       email: jd@mail.com
+ *       password: Password123
+ *       confirmPassword: Password123
+ */
+publicRouter.post("/api/employee/register", registerEmployeeController);
+/**
  * @swagger
- * /books:
- *   get:
- *     description: Get all books
- *     responses:
- *       200:
- *         description: Success
+ * /api/employee/login:
  *
  */
-
-publicRouter.post("/api/employee/register", registerEmployeeController);
 publicRouter.post("/api/employee/login", loginEmployeeController);
 
 export { publicRouter };
