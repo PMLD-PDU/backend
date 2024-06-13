2089ed094278c9f57c308aefcf310f3ee778a525
herjatiaji <herjati.aji@mail.ugm.ac.id>
2024-06-13 15:42:49 +0700

    notifications revise

--- src/service/notification.service.js
+++ src/service/notification.service.js
@@ -1,43 +1,22 @@
-import { prismaClient } from "../app/database";
+// services/notificationService.js
 
-export const addNotificationService = async (request) => {
-    const { wellId, title, message } = request.body;
+import { prismaClient } from '../app/database.js';
 
+export const sendNotificationService = async ({ wellId, title, message }) => {
     const well = await prismaClient.well.findUnique({
         where: { id: wellId },
     });
 
     if (!well) {
-        throw new Error("Well not found");
+        throw new Error('Well not found');
     }
 
-    // Create the notification
     const notification = await prismaClient.notification.create({
         data: {
             title,
             message,
             wellId,
         },
-    });
-
-    return notification;
-};
-
-export const getNotificationService = async (request) => {
-    const { wellId } = request.query;
-
-    const notifications = await prismaClient.notification.findMany({
-        where: { wellId },
-    });
-
-    return notifications;
-};
-
-export const deleteNotificationService = async (request) => {
-    const { notificationId } = request.body;
-
-    const notification = await prismaClient.notification.delete({
-        where: { id: notificationId },
     });
 
     return notification;
